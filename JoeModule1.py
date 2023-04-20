b569fdc20cd059ab37c794549ca179f683dfd23a
Weaverbeaver <123401142+Weaverbeaver@users.noreply.github.com>
2023-04-20 14:17:53 +0100

    correcting grammar mistake

--- JoeModule1.py
+++ JoeModule1.py
@@ -9,6 +9,6 @@ age = input('Please enter your age')
 print(username,'is',age,'years old')
 
 length = int(input('Please enter the length of the rectangle'))
-width = int(input('Please enter width of the rectangle'))
+width = int(input('Please enter the width of the rectangle'))
 print('The area of the rectangle is',length * width)
 print('The perimeter of the rectangle is',length + width)
