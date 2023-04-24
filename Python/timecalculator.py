8edcbab00298f6621d437e1400a755c5291eebf1
Weaverbeaver <123401142+Weaverbeaver@users.noreply.github.com>
2023-04-24 16:40:00 +0100

    Update timecalculator.py

--- Python/timecalculator.py
+++ Python/timecalculator.py
@@ -1,89 +1,98 @@
-# TimeCalculator.py
-
-# function to convert time string to integer components
-def convert_time(time_str):
-    parts = time_str.split(':')
-    day = int(parts[0])
-    hour = int(parts[1])
-    minute = int(parts[2])
-    return day, hour, minute
-
-# function to convert integer components to time string
-def convert_string(day, hour, minute):
-    time_str = f"{hour:02}:{minute:02}"
-    if day > 0:
-        time_str = f"{day:02}:{time_str}"
-    return time_str
-
-# function to add two times
-def add_time(time1, time2):
-    day1, hour1, minute1 = convert_time(time1)
-    day2, hour2, minute2 = convert_time(time2)
-    carry = 0
-    minute_sum = minute1 + minute2
-    if minute_sum >= 60:
-        carry = 1
-        minute_sum -= 60
-    hour_sum = hour1 + hour2 + carry
-    carry = 0
-    if hour_sum >= 24:
-        carry = 1
-        hour_sum -= 24
-    day_sum = day1 + day2 + carry
-    return convert_string(day_sum, hour_sum, minute_sum)
-
-# function to subtract two times
-def subtract_time(time1, time2):
-    day1, hour1, minute1 = convert_time(time1)
-    day2, hour2, minute2 = convert_time(time2)
-    carry = 0
-    minute_diff = minute1 - minute2
-    if minute_diff < 0:
-        carry = 1
-        minute_diff += 60
-    hour_diff = hour1 - hour2 - carry
-    carry = 0
-    if hour_diff < 0:
-        carry = 1
-        hour_diff += 24
-    day_diff = day1 - day2 - carry
-    if day_diff < 0:
-        return "Invalid: Result is negative"
-    return convert_string(day_diff, hour_diff, minute_diff)
-
-# function to multiply a time by a scalar
-def multiply_time(time_str, scalar):
-    day, hour, minute = convert_time(time_str)
-    total_minutes = day * 24 * 60 + hour * 60 + minute
-    result_minutes = total_minutes * scalar
-    day_result, remainder = divmod(result_minutes, 24 * 60)
-    hour_result, minute_result = divmod(remainder, 60)
-    return convert_string(day_result, hour_result, minute_result)
-
-# function to display the menu and get user choice
-def get_choice():
-    print("Main Menu:")
-    print("1. Add two times")
-    print("2. Subtract two times")
-    print("3. Multiply a time by a scalar")
-    print("9. Quit")
-    choice = input("Enter your choice (1-3 or 9): ")
-    return choice
-
-# main program
-time1 = input("Enter the first time (DD:HH:MM): ")
-time2 = input("Enter the second time (DD:HH:MM): ")
-
-choice = get_choice()
-while choice != "9":
-    if choice == "1":
-        print("Result:", add_time(time1, time2))
-    elif choice == "2":
-        print("Result:", subtract_time(time1, time2))
-    elif choice == "3":
-        scalar = int(input("Enter the scalar: "))
-        print("Result:", multiply_time(time1, scalar))
-    else:
-        print("Invalid choice!")
+while True:
+    print("Time Calculator")
+    print("1- Add 2 date-times")
+    print("2- Find the difference between 2 date-times")
+    print("3- Convert datetime to Hours")
+    print("4- Convert datetime to Minutes")
+    print("5- Convert Minutes to date-time")
+    print("6- Convert Hours to date-time")
+    print("7- Exit")
     
-    choice = get_choice()
+    choice = input("Enter an option: ")
+    
+    if choice == "1":
+        while True:
+            date_time1 = input("Enter time 1 (in format DD:HH:MM): ")
+            t1 = date_time1.split(':') # [dd,hh,mm]
+            if len(t1) == 3 and 0 <= int(t1[0]) < 1000 and 0 <= int(t1[1]) < 24 and 0 <= int(t1[2]) < 60:
+                break
+            else:
+                print("Incorrect format or value entered. Please enter time in the format DD:HH:MM and make sure the values are valid - max days is 1000.")
+        while True:
+            date_time2 = input("Enter time 2 (in format DD:HH:MM): ")
+            t2 = date_time2.split(':')
+            if len(t2) == 3 and 0 <= int(t2[0]) < 1000 and 0 <= int(t2[1]) < 24 and 0 <= int(t2[2]) < 60:
+                break
+            else:
+                print("Incorrect format or value entered. Please enter time in the format DD:HH:MM and make sure the values are valid - max days is 1000.")
+        t1 = date_time1.split(':') # returns [dd,hh,mm]
+        t2 = date_time2.split(':')
+        days = int(t1[0]) + int(t2[0])
+        hours = int(t1[1]) + int(t2[1])
+        minutes = int(t1[2]) + int(t2[2])
+        if minutes >= 60:
+            minutes -= 60
+            hours += 1
+        if hours >= 24:
+            hours -= 24
+            days += 1
+        print(f"The sum of {date_time1} and {date_time2} is {days}:{hours}:{minutes}")
+    elif choice == "2":
+        date_time1 = input("Enter time 1 (in format DD:HH:MM): ")
+        date_time2 = input("Enter time 2 (in format DD:HH:MM): ")
+        t1 = date_time1.split(':')
+        t2 = date_time2.split(':')
+        days = int(t1[0]) - int(t2[0]) 
+        hours = int(t1[1]) - int(t2[1]) 
+        minutes = int(t1[2]) - int(t2[2]) 
+        if minutes < 0:
+            minutes += 60
+            hours -= 1
+        if hours < 0:
+            hours += 24
+            days -= 1
+        if days < 0:
+            # Swap
+            t1, t2 = t2, t1
+            days = -days
+            hours = -hours
+            minutes = -minutes
+            if minutes < 0:
+                minutes += 60
+                hours -= 1
+            if hours < 0:
+                hours += 24
+                days -= 1
+        print(f"The difference between {date_time1} and {date_time2} is {days}:{hours}:{minutes}")
+
+    elif choice == "3":
+        date_time = input("Enter time in the format dd:hh:ss: ")
+        t1 = date_time.split(':')
+        total_minutes = int(t1[0]) * 1440 + int(t1[1]) * 60 + int(t1[2])
+        total_hours = total_minutes // 60
+        print(f"The total hours in {date_time} is {total_hours}")
+
+    elif choice == "4":
+        date_time = input("Enter time in the format dd:hh:ss: ")
+        t1 = date_time.split(':')
+        total_minutes = (int(t1[0]) * 1440 + int(t1[1]) * 60 + int(t1[2]))
+        print(f"The total minutes in {date_time} is {total_minutes}")
+
+    elif choice == "5":
+        total_minutes = int(input("Enter total minutes: "))
+        days = total_minutes // 1440
+        hours = (total_minutes % 1440) // 60
+        minutes = total_minutes % 60
+        print(f"The time in {total_minutes} minutes is {days}:{hours}:{minutes}")
+
+    elif choice == "6":
+        total_hours = int(input("Enter total hours: "))
+        days = total_hours // 24
+        hours = total_hours % 24
+        minutes = 0
+        print(f"The time in {total_hours} hours is {days}:{hours}:{minutes}")
+        
+    elif choice == "7":
+        break
+    else:
+        print("Invalid choice. Please try again.")
